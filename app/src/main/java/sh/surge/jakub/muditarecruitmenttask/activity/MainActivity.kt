c7d05448b0e90ab3a201de9451e3600157127e17
Kuba <jakub.rogala.95@gmail.com>
2020-10-14 11:09:36 +0200

    Create Main Activity

---
+++ app/src/main/java/sh/surge/jakub/muditarecruitmenttask/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package sh.surge.jakub.muditarecruitmenttask.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import sh.surge.jakub.muditarecruitmenttask.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
